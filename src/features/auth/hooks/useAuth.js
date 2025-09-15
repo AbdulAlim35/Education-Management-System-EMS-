4d29d011a52fbb690568bf6fa74fb5b5d35c8a1d
Abdul Alim <abdulalimmisuk@gmail.com>
2025-09-15 06:31:27 +0600

    refactor: update environment variable for server URL and implement axios client for API requests

--- src/features/auth/hooks/useAuth.js
+++ src/features/auth/hooks/useAuth.js
@@ -1,27 +1,18 @@
 import { useForm } from "react-hook-form";
-import axios from "axios";
+import axiosClient from "@/api/httpClient";
 const useAuth = () => {
   const formControler = useForm();
 
   return {
     form: formControler,
-    onSubmit: async () => {
+    onSubmit: async (data) => {
       try {
-        const response = await axios.post(
-          "http://localhost:3000/login",
-          {
-            email: "akrmma@gmail.com",
-            password: "123456",
-          },
-          {
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        console.log("Response:", response.data);
+        const req = await axiosClient.post("login", {
+          ...data
+        });
+        
       } catch (error) {
-        console.error("Error:", error);
+        console.error("Error:", error.response);
       }
     },
   };

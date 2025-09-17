e56cbad5c50e6cf8f4f890cca31e52bbf3f8a668
Abdul Alim <abdulalimmisuk@gmail.com>
2025-09-17 12:09:28 +0600

    enhance PageLayout to accept dynamic form and header props; update StudentSection

--- src/features/student/pages/StudentSection.jsx
+++ src/features/student/pages/StudentSection.jsx
@@ -3,11 +3,13 @@ import { Outlet } from 'react-router-dom'
 import PageLayout from '@/components/share/PageLayout'
 import TableLayout from '@/components/share/TableLayout'
 import  tableConfig  from '../config/tableConfig'
+import { studentFields } from "@/features/student/config/studentFields";
+import AuthFormLayout from "@/components/share/AuthFormLayout";
 
 export default function StudentSection() {
   return (
    <>
-<PageLayout title="students" description="Manage student information and records" buttonLabel="Add Student">
+<PageLayout title="students" description="Manage student information and records" buttonLabel="Add Student" header="Add New Student" form={<AuthFormLayout studentFields={studentFields}/>}>
 <TableLayout filter={false}  columns={tableConfig} data={tableConfig} />
 </PageLayout>
    </>

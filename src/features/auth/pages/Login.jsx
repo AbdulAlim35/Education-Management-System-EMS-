9bffaab01f59c02caf0d6b99ff994a54460c3221
AbdulAlim35 <abdulalimmisuk@gmail.com>
2025-11-05 08:12:27 +0600

    fix: adjust layout and spacing in login page

--- src/features/auth/pages/Login.jsx
+++ src/features/auth/pages/Login.jsx
@@ -10,8 +10,8 @@ function Login() {
       className="min-h-screen flex items-center justify-center bg-blue-500 from-primary-600 to-primary-800"
     >
       <div className="max-w-md w-full mx-4">
-        <div className="bg-white rounded-2xl shadow-2xl p-8">
-          <div className="text-center mb-3">
+        <div className="bg-white rounded-2xl shadow-2xl p-8 ">
+          <div className="text-center mb-2">
             <div className="w-20 h-20 text-white text-4xl  bg-blue-500 rounded-full flex items-center justify-center mx-auto mb-4">
               <IoBookOutline />
             </div>
@@ -19,9 +19,9 @@ function Login() {
             <p className="text-gray-600 mt-2">Student Management System</p>
           </div>
           <FormLayout formData={formData} fields={fieldsData} />
-          <div className="flex flex-row  space-x-4 mt-1">
-             <p className="text-gray-600 mt-2">Email:admin@gmail.com</p>
-             <p className="text-gray-600 mt-2">Password:123456</p>
+          <div className="flex flex-row  justify-center space-x-4 mt-2">
+             <p className="text-gray-600 ">Email:admin@gmail.com</p>
+             <p className="text-gray-600 ">Password:123456</p>
           </div>
           
         </div>
